e5dc5f8752ff2ed27d57ed2a64c8d0c76353bb1c
betz <betzinbal@gmail.com>
2024-10-15 10:07:56 +0300

    first commit base and mpn complited

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { NotesModule } from './notes/notes.module';
+
+@Module({
+  imports: [UserModule, AuthModule, NotesModule],
+})
+export class AppModule {}
